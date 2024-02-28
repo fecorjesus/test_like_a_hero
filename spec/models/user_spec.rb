960d64df9a8d91665b9a184e4f21dacc1a70f05f
Felipe Correia de Jesus <fecorjesus@gmail.com>
2024-02-28 07:19:10 -0300

    teste

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "is invalid if the level is not between 1 and 99" do
-    user = build(:user, level: FFaker::Random.rand(1..99))
+    user = build(:user, level: FFaker::Random.rand(100..9999))
 
     expect(user).to_not be_valid
    end
